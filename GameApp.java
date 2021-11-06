ada77596120e8ddf15b2becad75309a8207e3cbf
ALEXANDROS PAPPAS <80106020+AlexandrosAueb@users.noreply.github.com>
2021-11-06 18:17:00 +0200

    Add files via upload

--- GameApp.java
+++ GameApp.java
@@ -1,8 +1,22 @@
+import java.awt.EventQueue;
+
 public class GameApp {
 
 	public static void main(String[] args) {
+		/**
+		 * Launch the application.
+		 */		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu window = new MainMenu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
-
 	}
 
 }
--- MainMenu.java
+++ MainMenu.java
@@ -19,7 +19,7 @@ import java.awt.SystemColor;
 
 public class MainMenu {
 
-	private JFrame frame;
+	JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -28,18 +28,7 @@ public class MainMenu {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainMenu window = new MainMenu();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the application.
